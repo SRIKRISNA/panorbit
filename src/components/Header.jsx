e34e439edbfab09a57f7da9511a3c2b742c00bda
Srikrishna <krishna333sp@gmail.com>
2023-02-13 18:38:28 +0530

    logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Header = (props) => {
+    let Navigate = useNavigate();
+
+    const logOut = () => {
+        localStorage.clear();
+        Navigate('/', {replace:true});
+    }
     return (
         <div className='header-container'>
             <div className='logo'>
                 <h3>Profile</h3>
             </div>
-            <div className="imgName">
-                <img src={props.pic} width="35px" id='pic' alt='pic'></img>
-                {props.name}
+            <div className="imgName" onClick={logOut}>
+               <abbr title='Logout' > <img src={props.pic} width="35px" id='pic' alt='pic'></img>
+                {props.name}</abbr>
             </div>
         </div>
     )
